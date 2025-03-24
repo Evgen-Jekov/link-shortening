104e94415c6c4370112ae604a7d42531672a875d
Evgen-Jekov <zekovevgen992@gmail.com>
2025-03-24 01:38:12 +0000

    link shortening

---
+++ create_app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api
+
+def create_app():
+    app = Flask(__name__)
+    api = Api(app)
+    CORS(app)
+
+    return app
